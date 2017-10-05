56d386b2627b24afd84ec8ace878ddb3a500a92e
sparck75 <dominic.lavoie@gmail.com>
2017-10-05 18:44:19 -0400

    Changed container

--- create_hassio_addon.sh
+++ create_hassio_addon.sh
@@ -6,7 +6,7 @@ DOCKER_PUSH="true"
 DOCKER_CACHE="true"
 DOCKER_WITH_LATEST="true"
 BRANCH=master
-REPOSITORY=https://github.com/sparck75/ha-addons
+REPOSITORY=https://github.com/home-assistant/hassio-addons
 
 cleanup() {
     echo "[INFO] Cleanup."
