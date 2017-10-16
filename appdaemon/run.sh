4c327d15a3f8ad299bb722a03bdd7ed4b8cb94ab
sparck75 <dominic.lavoie@gmail.com>
2017-10-16 15:05:08 -0400

    New launch options

--- appdaemon/run.sh
+++ appdaemon/run.sh
@@ -3,24 +3,18 @@ set -e
 
 CONFIG_PATH=/data/options.json
 CONFIG_DIR=/config/hadaemon
-EXTRA_CMD="-D DEBUG"
-
 
 DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
+DOMAIN_SSL=$(jq --raw-output ".domain_ssl" $CONFIG_PATH)
+EXTRA_CMD=$(jq --raw-output ".arguments" $CONFIG_PATH)
 
 if [ ! -d $CONFIG_DIR ]; then
 	mkdir -p $CONFIG_DIR
 	cp /etc/appdaemon-example.yaml $CONFIG_DIR/appdaemon.yaml
+	cp /etc/apps-example.yaml $CONFIG_DIR/apps.yaml
 fi
 
-echo 0.0.0.0 $DOMAIN >> /etc/hosts
-echo "Starting my appdaemon test for HA-addons"
-echo "Config: " $CONFIG_DIR
-echo "Extra: " $EXTRA_CMD
-echo "Domain: " $DOMAIN
-pwd
-ls -l /usr/src/app
-ls -l 
-ls /config/hadaemon
+echo 0.0.0.0 $DOMAIN_SSL >> /etc/hosts
+
 appdaemon -c $CONFIG_DIR $EXTRA_CMD
 
