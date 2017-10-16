54d42c7a9cf587e716f1d947cd05a7bf5a37e04e
sparck75 <dominic.lavoie@gmail.com>
2017-10-16 15:28:59 -0400

    Cleanup

--- appdaemon/run.sh
+++ appdaemon/run.sh
@@ -3,10 +3,9 @@ set -e
 
 CONFIG_PATH=/data/options.json
 CONFIG_DIR=/config/hadaemon
+EXTRA_CMD="-D DEBUG"
 
 DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
-DOMAIN_SSL=$(jq --raw-output ".domain_ssl" $CONFIG_PATH)
-EXTRA_CMD=$(jq --raw-output ".arguments" $CONFIG_PATH)
 
 if [ ! -d $CONFIG_DIR ]; then
 	mkdir -p $CONFIG_DIR
@@ -14,7 +13,7 @@ if [ ! -d $CONFIG_DIR ]; then
 	cp /etc/apps-example.yaml $CONFIG_DIR/apps.yaml
 fi
 
-echo 0.0.0.0 $DOMAIN_SSL >> /etc/hosts
+echo 0.0.0.0 $DOMAIN >> /etc/hosts
 
 appdaemon -c $CONFIG_DIR $EXTRA_CMD
 
