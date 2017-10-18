2aebec2bb89f74ccb673c2b4dff5e272e7de44b9
sparck75 <dominic.lavoie@gmail.com>
2017-10-18 10:01:58 -0400

    fix

--- appdaemon/run.sh
+++ appdaemon/run.sh
@@ -3,9 +3,6 @@ set -e
 
 CONFIG_PATH=/data/options.json
 CONFIG_DIR=/config/hadaemon
-#EXTRA_CMD=""
-#EXTRA_CMD2=""
-
 
 DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
 COMMTYPE=$(jq --raw-output ".commtype" $CONFIG_PATH)
@@ -17,19 +14,17 @@ if [ ! -d $CONFIG_DIR ]; then
 	cp /etc/apps-example.yaml $CONFIG_DIR/apps.yaml
 fi
 
-if [ "$DEBUGGING" = "DEBUG" ]; then
-	EXTRA_CMD = "-D DEBUG"
-	else
-	  EXTRA_CMD = " "
+if [ $DEBUGGING = "DEBUG" ]; then
+  EXTRA_CMD = "-D DEBUG"
+else
+  EXTRA_CMD = " "
 fi
 
-if [ "$COMMTYPE" == "SSE" ]; then
-	EXTRA_CMD2 = "--commtype:SSE "
-	else
-	  EXTRA_CMD2 = " "
+if [ $COMMTYPE == "SSE" ]; then
+  EXTRA_CMD2 = "--commtype:SSE "
+else
+  EXTRA_CMD2 = " "
 fi
-
-
 echo 0.0.0.0 $DOMAIN >> /etc/hosts
 
 appdaemon -c $CONFIG_DIR $EXTRA_CMD $EXTRA_CMD2
